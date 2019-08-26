bda1a5cbed63a29fcc3c36e7809138d332588a7d
lil-Dwan <alex.cap.an@gmail.com>
2019-08-26 11:45:02 -0400

    first commit

---
+++ frankie_greet/bot.js
@@ -0,0 +1,17 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(auth.token);
+
+
+client.on('message', msg => {
+    if (msg.content === 'kamehame') {
+      msg.reply('HAAAAA!!!');
+    }
+  });
